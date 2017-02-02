4a447183c99c0948a8e2b8491ad12fc2c695606c
Nitin Reddy <reddynitin3@gmail.com>
2017-02-02 23:36:34 +0530

    Simplified code

--- test/components/UserList.test.js
+++ test/components/UserList.test.js
@@ -1,18 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { shallow } from 'enzyme';
+import { mount } from 'enzyme';
 import UserList from 'UserList';
 import Constants from '../../app/javascript/constants/app-constant.js';
 
 describe('UserList', () => {
     it('renders without crashing', () => {
-        const div = document.createElement('div');
-        ReactDOM.render(<UserList/>, div);
+       mount(<UserList/>);
     });
 
     describe('getUpdatedlastUserId', () => {
         it('should return userId of last user in the array', () => {
-            const userList = shallow(<UserList/>);
+            const userList = mount(<UserList/>);
             let users = Constants.GIT_USERS;
             let expected = 30;
             let actual = userList.instance().getUpdatedlastUserId(users);
